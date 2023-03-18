56b58b826f5932a527c980adaf0046ea52ad567b
美絵瑠 <122779206+Miel-shinonome@users.noreply.github.com>
2023-03-18 18:13:53 +0900

    Update messaging-room.vue

--- packages/client/src/pages/messaging/messaging-room.vue
+++ packages/client/src/pages/messaging/messaging-room.vue
@@ -10,7 +10,7 @@
 			<MkPagination v-if="pagination" ref="pagingComponent" :key="userAcct || groupId" :pagination="pagination">
 				<template #empty>
 					<div class="_fullinfo">
-						<img src="https://xn--931a.moe/assets/info.jpg" class="_ghost"/>
+						<img src="https://raw.githubusercontent.com/Miel-shinonome/png/main/nana_info.png" class="_ghost"/>
 						<div>{{ i18n.ts.noMessagesYet }}</div>
 					</div>
 				</template>
