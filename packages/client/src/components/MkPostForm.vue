9cdc1f588ee187c9289754d0d31dcf24ec57e78d
美絵瑠 <122779206+Miel-shinonome@users.noreply.github.com>
2024-03-13 01:32:43 +0900

    Update MkPostForm.vue

--- packages/client/src/components/MkPostForm.vue
+++ packages/client/src/components/MkPostForm.vue
@@ -14,7 +14,7 @@
 		</button>
 		<div class="right">
 			<span class="text-count" :class="{ over: textLength > maxTextLength }">{{ maxTextLength - textLength }}</span>
-			<span v-if="localOnly" class="local-only"><i class="fas fa-atom_symbol"></i></span>
+			<span v-if="localOnly" class="local-only"><i class="fas fa-star_and_crescent"></i></span>
 			<button ref="visibilityButton" v-tooltip="i18n.ts.visibility" class="_button visibility" :disabled="channel != null" @click="setVisibility">
 				<span v-if="visibility === 'public'"><i class="fas fa-globe"></i></span>
 				<span v-if="visibility === 'home'"><i class="fas fa-home"></i></span>
